5671d673221b6bf5fddda89d52f7e7da43aedd0e
Artur Poznański <apoznan5@gmail.com>
2025-04-10 20:10:48 +0200

    using removed

--- Group.cs
+++ Group.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Week5Lesson27
+﻿namespace Week5Lesson27
 {
     public class Group
     {
